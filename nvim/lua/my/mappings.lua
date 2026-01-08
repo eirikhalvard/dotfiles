4c1eafe6af6df225fdd1692ac9fcd6738249342c
Eirik Halvard Sæther <eirik.saether@bidbax.no>
2026-01-08 16:28:34 +0100

    update nvim config + other stuff

--- nvim/lua/my/mappings.lua
+++ nvim/lua/my/mappings.lua
@@ -1,190 +1,110 @@
 local wk = require("which-key")
 
--- === Leader Key Mappings === --
-wk.register({
-	name = "🌍 Leader",
-	[" "] = { "<cmd>Telescope commands<CR>", "commands" },
-	[","] = { "<cmd>e #<CR>", "previous-file" },
-	c = { name = "🛠  Compile", a = "async-compile", c = "compile" },
-	d = {
-		name = "🎮 Do (Action)",
-		f = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "code-action" },
-		h = "insert-haskell-expression",
-		i = { "<cmd>luafile ~/.config/nvim/init.lua<CR>", "source-init" },
-		s = { "<cmd>syntax sync fromstart<CR>", "sync-syntax" },
-	},
-	e = {
-		name = "💻 Edits",
-		f = { "z=1<CR>", "fix-typo" },
-		n = "norm-lines",
-		N = "norm-non-empty-lines",
-		r = "raise-word-to-parenexp",
-		s = "substitute",
-		S = "substitute-thing",
-	},
-	f = {
-		name = "💿 Files",
-		b = { "<cmd>Telescope buffers<CR>", "buffers" },
-		c = { "<cmd>Telescope find_files search_dirs=~/.config<CR>", "config-files" },
-		f = { "<cmd>Telescope find_files<CR>", "files" },
-		F = { "<cmd>Telescope file_browser<CR>", "files" },
-		g = {
-			name = "🌳 Git",
-			b = { "<cmd>Telescope git_branches<CR>", "git-branches" },
-			f = { "<cmd>Telescope git_files<CR>", "git-files" },
-			j = { "<cmd>Telescope git_stash<CR>", "git-stash" },
-			s = { "<cmd>Telescope git_status<CR>", "git-status" },
-		},
-		H = { "<cmd>tabfirst<CR>", "first-tab" },
-		h = { "<cmd>tabprevious<CR>", "previous-tab" },
-		J = { "<cmd>blast<CR>", "last-buffer" },
-		j = { "<cmd>bnext<CR>", "next-buffer" },
-		K = { "<cmd>bfirst<CR>", "first-buffer" },
-		k = { "<cmd>bprevious<CR>", "previous-buffer" },
-		L = { "<cmd>tablast<CR>", "last-tab" },
-		l = { "<cmd>tabnext<CR>", "next-tab" },
-		N = { "<cmd>clast<CR>", "last-quickfix" },
-		n = { "<cmd>cnext<CR>", "next-quickfix" },
-		o = { "<cmd>Telescope oldfiles<CR>", "oldfiles" },
-		P = { "<cmd>cfirst<CR>", "first-quickfix" },
-		p = { "<cmd>cprevious<CR>", "previous-quickfix" },
-		s = { "<cmd>Telescope grep_string<CR>", "search" },
-		t = { "<cmd>Telescope<CR>", "telescope" },
-		y = { "<cmd>Telescope grep_string<CR>", "yank-and-search" },
-		z = { "<cmd>lua require'telescope'.extensions.z.list{}<CR>", "telescope-z" },
-
-	},
-	g = {
-		name = "🐙 Git",
-		b = { "<cmd>Git blame<CR>", "blame" },
-		c = { "<cmd>Git commit<CR>", "commit" },
-		f = { "<cmd>Git fetch<CR>", "fetch" },
-		g = { "<cmd>Git<CR>", "git-menu" },
-		h = { "<cmd>!gh repo view -w<CR>", "browse-github" },
-		l = { "<cmd>Git pull<CR>", "pull" },
-		p = { "<cmd>Git push<CR>", "push" },
-		s = { "<cmd>GitGutterStageHunk<CR>", "stage-hunk" },
-		u = { "<cmd>GitGutterUndoHunk<CR>", "undo-hunk" },
-		w = { "<cmd>Gwrite<CR>", "stage-file" },
-	},
-  n = { "<cmd>e ~/Drive/Diverse/notes.md<CR>" , "🗒 Notes" },
-	h = {
-		name = "🏳️  Help",
-		h = { "<cmd>Telescope help_tags<CR>", "help" },
-		s = { "<cmd>Telescope spell_suggest<CR>", "spell-suggest" },
-		m = { "<cmd>Telescope man_pages<CR>", "man-pages" },
-	},
-	o = {
-		name = "🛹 Open",
-		b = { "<cmd>e ~/.config/shell/bash_profile<CR>", "bash-profile" },
-		f = "ftplugin-filetype",
-		i = { '<cmd>lua require("telescope.builtin").find_files({path_display = function(opts, path) return string.gsub(require("telescope.utils").path_tail(path), "/Users/n647546//.config/nvim/lua/my/mappings.lua.lua", "") end, search_dirs= {"~/.config/nvim/lua"}})<CR>', "config-files" },
-		-- s = { '<cmd>UltiSnipsEdit<CR>', 'open-snippets' },
-	},
-	Q = { "<cmd>q!<CR>", "force-quit-file" },
-	q = { "<cmd>q<CR>", "quit-file" },
-	r = {
-		name = "🦀 Resources",
-		s = {
-			name = "🐸 Slime",
-			b = { ",rsmab", "send-bracket" },
-			c = { "<Plug>SlimeConfig", "config" },
-			m = { "<Plug>SlimeMotionSend", "send-motion" },
-			p = { "<Plug>SlimeParagraphSend", "send-paragraph" },
-			q = { ",rsmiq", "send-inside-quotes" },
-			s = "send",
-		},
-	},
-	s = {
-		name = "🚀 LSP",
-    a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "code-action" },
-		e = { "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", "show-line-diagnostics" },
-		D = { "<cmd>lua vim.lsp.buf.type_definition()<CR>", "type-definitions" },
-		f = { "<cmd>lua vim.lsp.buf.format { async = true }<CR>", "formatting" },
-		i = { "<cmd>TroubleToggle document_diagnostics<CR>", "trouble-document" },
-		j = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "goto-next" },
-		k = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", "goto-prev" },
-		l = { "<cmd>LuaSnipListAvailable<CR>", "lua-snippet-list" },
-		o = { "<cmd>TroubleToggle workspace_diagnostics<CR>", "trouble-workspace" },
-		q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", "set-loclist" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "rename" },
-		s = { "<cmd>TroubleToggle<CR>", "trouble-toggle" },
-		u = { "<cmd>TroubleToggle quickfix<CR>", "trouble-quickfix" },
-		wa = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "add-workspace-folder" },
-		wr = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "remove-workspace-folder" },
-		wl = { "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "list-workspace-folders" },
-	},
-	t = {
-		name = "🔘 Toggles",
-		g = { "<cmd>Goyo<CR>", "goyo" },
-		s = {
-			name = "📝 Spell",
-			e = { "<cmd>set spelllang=en<CR>", "english" },
-			n = { "<cmd>set spelllang=nb<CR>", "norwegian" },
-			s = { "<cmd>set spell!<CR>", "toggle-spell" },
-		},
-		t = { "<cmd>NERDTreeFind<CR>", "tree" },
-		w = { "<cmd>set list!<CR>", "whitespace" },
-	},
-	w = { "<cmd>w<CR>", "write-file" },
-}, {
-	prefix = "<leader>",
+wk.setup({
+  icons = { mappings = false },
 })
 
--- " Some commands are to complex to be written directly in
--- " the which-key map. Either there is an ex command that
--- " uses more complex features, or we have custom implementation
--- " for visual mode and normal mode
-vim.api.nvim_set_keymap("n", "<leader>dh", [[:read !ghead -n -1 < <(ghc -e "")<Left><Left>]], { noremap = false })
-vim.api.nvim_set_keymap("n", "<leader>ca", [[:w! | AsyncRun compiler "%"<CR>]], { noremap = false })
-vim.api.nvim_set_keymap("n", "<leader>cc", [[:w! | !compiler "<C-r>%"<CR>]], { noremap = false })
-vim.api.nvim_set_keymap("n", "<leader>er", "diwvabp", { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>er", "dvabp", { noremap = true })
-vim.api.nvim_set_keymap("", "<leader>en", ":g/.*/norm ", { noremap = false })
-vim.api.nvim_set_keymap("", "<leader>eN", ":g/..*/norm ", { noremap = false })
-vim.api.nvim_set_keymap("n", "<leader>es", ":%s//g<Left><Left>", { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>es", [[:s/\%V/g<Left><Left>]], { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>eS", [[yiw:%s/\<<C-r>"\>//g<Left><Left>]], { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>eS", [[y:%s/\<<C-r>"\>//g<Left><Left>]], { noremap = true })
-vim.api.nvim_set_keymap(
-	"",
-	"<leader>of",
-	[[:e ~/.config/nvim/ftplugin/<C-r>=&filetype<CR>.vim<CR>]],
-	{ noremap = false }
+wk.add({
+  { "<leader>", group = "🌍 Leader" },
+
+  { "<leader><space>", "<cmd>Telescope commands<cr>", desc = "Commands" },
+  { "<leader>,", "<cmd>e #<cr>", desc = "Previous file" },
+
+  { "<leader>c", group = "🛠 Compile" },
+  { "<leader>ca", desc = "Async compile" },
+  { "<leader>cc", desc = "Compile" },
+
+  { "<leader>d", group = "🎮 Do" },
+  { "<leader>df", vim.lsp.buf.code_action, desc = "Code action" },
+  { "<leader>di", "<cmd>luafile ~/.config/nvim/init.lua<cr>", desc = "Source init" },
+  { "<leader>ds", "<cmd>syntax sync fromstart<cr>", desc = "Sync syntax" },
+
+  { "<leader>e", group = "💻 Edits" },
+  { "<leader>ef", "z=1", desc = "Fix typo" },
+  { "<leader>es", desc = "Substitute" },
+  { "<leader>eS", desc = "Substitute word" },
+
+  { "<leader>f", group = "💿 Files" },
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files" },
+  { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+  { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Old files" },
+  { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Search" },
+  { "<leader>fz", "<cmd>Telescope z<cr>", desc = "Z" },
+
+  { "<leader>g", group = "🐙 Git" },
+  { "<leader>gg", "<cmd>Git<cr>", desc = "Git status" },
+  { "<leader>gb", "<cmd>Git blame<cr>", desc = "Blame" },
+  { "<leader>gp", "<cmd>Git push<cr>", desc = "Push" },
+
+  { "<leader>h", group = "🏳️ Help" },
+  { "<leader>hh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+
+  { "<leader>o", group = "🛹 Open" },
+  {
+    "<leader>of",
+    function()
+      vim.cmd("e ~/.config/nvim/ftplugin/" .. vim.bo.filetype .. ".vim")
+    end,
+    desc = "ftplugin file",
+  },
+
+  { "<leader>s", group = "🚀 LSP" },
+  { "<leader>sa", vim.lsp.buf.code_action, desc = "Code action" },
+  { "<leader>sr", vim.lsp.buf.rename, desc = "Rename" },
+  { "<leader>sf", function() vim.lsp.buf.format({ async = true }) end, desc = "Format" },
+  { "<leader>ss", "<cmd>TroubleToggle<cr>", desc = "Trouble" },
+
+  { "<leader>t", group = "🔘 Toggles" },
+  { "<leader>tg", "<cmd>Goyo<cr>", desc = "Goyo" },
+  { "<leader>tw", "<cmd>set list!<cr>", desc = "Whitespace" },
+
+  { "<leader>w", "<cmd>w<cr>", desc = "Write" },
+  { "<leader>q", "<cmd>q<cr>", desc = "Quit" },
+  { "<leader>Q", "<cmd>q!<cr>", desc = "Force quit" },
+})
+
+vim.keymap.set("n", "<leader>dh",
+  [[:read !ghead -n -1 < <(ghc -e "")<Left><Left>]],
+  { desc = "Insert Haskell expr" }
 )
-vim.api.nvim_set_keymap("x", "<leader>rss", "<Plug>SlimeRegionSend", { noremap = false })
-vim.api.nvim_set_keymap("n", "<leader>rss", "<Plug>SlimeParagraphSend", { noremap = false })
 
--- " === General Mappings === "
+vim.keymap.set("n", "<leader>ca",
+  ":w! | AsyncRun compiler \"%\"<cr>",
+  { desc = "Async compile" }
+)
 
--- Move up/down editor lines
-vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true })
-vim.api.nvim_set_keymap("n", "gj", "j", { noremap = true })
-vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true })
-vim.api.nvim_set_keymap("n", "gk", "k", { noremap = true })
+vim.keymap.set("n", "<leader>cc",
+  ":w! | !compiler \"%\"<cr>",
+  { desc = "Compile" }
+)
 
--- Y behaves the same was as D, C, etc
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>er", "dvabp", { desc = "Raise word" })
 
--- Use arrow keys to navigate files / tmux panes
-vim.api.nvim_set_keymap("", "<Left>", "<C-h>", { noremap = false })
-vim.api.nvim_set_keymap("", "<Right>", "<C-l>", { noremap = false })
-vim.api.nvim_set_keymap("", "<Down>", "}j", { noremap = false })
-vim.api.nvim_set_keymap("", "<Up>", "{k", { noremap = false })
+vim.keymap.set("x", "<leader>rss", "<Plug>SlimeRegionSend", { desc = "Slime region" })
+vim.keymap.set("n", "<leader>rss", "<Plug>SlimeParagraphSend", { desc = "Slime paragraph" })
 
-vim.api.nvim_set_keymap("n", "øf", "<leader>ff", { noremap = false })
-vim.api.nvim_set_keymap("n", "øs", "<leader>fs", { noremap = false })
-vim.api.nvim_set_keymap("n", "ør", "<leader>fo", { noremap = false })
+-- Better movement
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "gj", "j")
+vim.keymap.set("n", "gk", "k")
 
--- Mappings for iTerm2
-vim.api.nvim_set_keymap("n", "<F10>", ":w<CR>", { noremap = false }) -- CMD-s
-vim.api.nvim_set_keymap("i", "<F10>", "<C-O>:w<CR>", { noremap = false }) -- CMD-s
+-- Yank to end of line
+vim.keymap.set("n", "Y", "y$")
+
+-- Arrow keys → tmux / splits
+vim.keymap.set("", "<Left>", "<C-h>")
+vim.keymap.set("", "<Right>", "<C-l>")
+vim.keymap.set("", "<Up>", "{k")
+vim.keymap.set("", "<Down>", "}j")
+
+-- CMD-S (iTerm)
+vim.keymap.set({ "n", "i" }, "<F10>", "<cmd>w<cr>", { desc = "Save" })
 
 -- LSP
-vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "gR", "<cmd>Telescope lsp_references<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "gr", "<cmd>TroubleToggle lsp_references<CR>", { noremap = true })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Implementation" })
+vim.keymap.set("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { desc = "References" })
--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -12,17 +12,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-
--- lazy packages
-
 require("lazy").setup({
 	-- === Core functionality ===--
 	"tpope/vim-commentary",
 	"tpope/vim-surround",
 	"tpope/vim-repeat",
 	"junegunn/vim-easy-align",
-  { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
+  	{ "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
 	"michaeljsmith/vim-indent-object",
 	"tommcdo/vim-exchange",
 	"wellle/targets.vim",
@@ -30,38 +26,234 @@ require("lazy").setup({
 	"folke/which-key.nvim",
 	"nvim-lua/plenary.nvim",
 	"nvim-lua/popup.nvim",
-  { 'nvim-treesitter/nvim-treesitter', cmd = 'TSUpdate' },
-
 	-- === Editor ===--
-	{ "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
-	"junegunn/fzf.vim",
-	"nvim-telescope/telescope.nvim",
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	"nvim-telescope/telescope-z.nvim",
-  "ryanoasis/vim-devicons",
+
+	{
+	  "hrsh7th/nvim-cmp",
+	  dependencies = {
+	    "hrsh7th/cmp-nvim-lsp",
+	    "hrsh7th/cmp-nvim-lua",
+	    "hrsh7th/cmp-buffer",
+	    "hrsh7th/cmp-path",
+	    "hrsh7th/cmp-nvim-lsp-signature-help",
+	    "L3MON4D3/LuaSnip",
+	    "saadparwaiz1/cmp_luasnip",
+	    "onsails/lspkind-nvim",
+	  },
+	  config = function()
+	    local cmp = require("cmp")
+	    local luasnip = require("luasnip")
+	    local lspkind = require("lspkind")
+
+	    lspkind.init({ mode = "symbol_text", preset = "default" })
+	    require("luasnip.loaders.from_vscode").lazy_load()
+
+	    local has_words_before = function()
+	      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	    end
+
+	    cmp.setup({
+	      snippet = {
+	        expand = function(args)
+	          luasnip.lsp_expand(args.body)
+	        end,
+	      },
+	      completion = { completeopt = "menu,menuone,noinsert,noselect" },
+	      mapping = {
+	        ["<C-p>"] = cmp.mapping.select_prev_item(),
+	        ["<C-n>"] = cmp.mapping.select_next_item(),
+	        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+	        ["<C-Space>"] = cmp.mapping.complete(),
+	        ["<C-e>"] = cmp.mapping.close(),
+	        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+	        ["<Tab>"] = cmp.mapping(function(fallback)
+	          if luasnip.expand_or_jumpable() then
+	            luasnip.expand_or_jump()
+	          elseif has_words_before() then
+	            cmp.complete()
+	          else
+	            fallback()
+	          end
+	        end, { "i", "s" }),
+	        ["<S-Tab>"] = cmp.mapping(function(fallback)
+	          if luasnip.jumpable(-1) then
+	            luasnip.jump(-1)
+	          else
+	            fallback()
+	          end
+	        end, { "i", "s" }),
+	      },
+	      formatting = {
+	        format = lspkind.cmp_format({
+	          mode = "symbol_text",
+	          maxwidth = 50,
+	          symbol_map = {
+	            Text = "", Method = "", Function = "", Constructor = "",
+	            Field = "ﰠ", Variable = "", Class = "ﴯ", Interface = "",
+	            Module = "", Property = "ﰠ", Unit = "塞", Value = "",
+	            Enum = "", Keyword = "", Snippet = "", Color = "",
+	            File = "", Reference = "", Folder = "", EnumMember = "",
+	            Constant = "", Struct = "פּ", Event = "", Operator = "",
+	          },
+	        }),
+	      },
+	      sources = {
+	        { name = "nvim_lsp" },
+	        { name = "nvim_lsp_signature_help" },
+	        { name = "nvim_lua" },
+	        { name = "luasnip" },
+	        { name = "path" },
+	        { name = "buffer" },
+	      },
+	    })
+	  end,
+	},
+
+	-- LSP
+	{
+	  "neovim/nvim-lspconfig",
+	  config = function()
+	    local lsp = vim.lsp
+
+	    -- Haskell
+	    lsp.config("hls", {
+	      cmd = { "haskell-language-server-wrapper", "--lsp" },
+	      filetypes = { "haskell", "lhaskell" },
+	      root_markers = {
+	        "cabal.project",
+	        "stack.yaml",
+	        "package.yaml",
+	        "*.cabal",
+	      },
+	      settings = {
+	        haskell = {
+	          formattingProvider = "fourmolu",
+	        },
+	      },
+	    })
+
+	    -- TypeScript
+	    lsp.config("tsserver", {
+	      cmd = { "typescript-language-server", "--stdio" },
+	      filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	      root_markers = { "package.json", "tsconfig.json", ".git" },
+	    })
+
+	    -- Python
+	    lsp.config("pylsp", {
+	      cmd = { "pylsp" },
+	      filetypes = { "python" },
+	      root_markers = { "pyproject.toml", "setup.py", ".git" },
+	    })
+
+	    -- JSON
+	    lsp.config("jsonls", {
+	      cmd = { "vscode-json-language-server", "--stdio" },
+	      filetypes = { "json", "jsonc" },
+	    })
+
+	    -- Bash
+	    lsp.config("bashls", {
+	      cmd = { "bash-language-server", "start" },
+	      filetypes = { "sh", "bash" },
+	    })
+
+	    -- Auto-start LSPs
+	    vim.api.nvim_create_autocmd("FileType", {
+	      callback = function()
+	        lsp.start({
+	          name = vim.bo.filetype,
+	        })
+	      end,
+	    })
+	  end,
+	},
+
+	-- Treesitter
+	{
+	  "nvim-treesitter/nvim-treesitter",
+	  build = ":TSUpdate",
+	  event = { "BufReadPost", "BufNewFile" },
+	  config = function()
+	    require("nvim-treesitter.configs").setup({
+	      ensure_installed = {
+	        "haskell", "elm", "lua", "python", "r", "scala",
+	        "json", "markdown", "dockerfile", "yaml",
+	        "bash", "vim", "css", "html",
+	      },
+	      highlight = {
+	        enable = true,
+	        additional_vim_regex_highlighting = false,
+	      },
+	      incremental_selection = {
+	        enable = true,
+	        keymaps = {
+	          init_selection = "øv",
+	          node_incremental = "øf",
+	          scope_incremental = "øg",
+	          node_decremental = "ød",
+	        },
+	      },
+	    })
+	  end,
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+		"nvim-telescope/telescope-z.nvim",
+		"folke/trouble.nvim",  -- make sure trouble is available
+		},
+		config = function()
+		local actions = require("telescope.actions")
+		local trouble = require("trouble.sources.telescope")
+
+		require("telescope").setup({
+			defaults = {
+			layout_strategy = "vertical",
+			mappings = {
+				i = { ["<c-t>"] = trouble.open },
+				n = { ["<c-t>"] = trouble.open },
+			},
+			path_display = { "smart" },
+			},
+			extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+			},
+		})
+
+		-- load extensions after setup
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("z")
+		end,
+	},
+
+
+  	"ryanoasis/vim-devicons",
 	"scrooloose/nerdtree",
 	"jpalardy/vim-slime",
 	"vim-scripts/paredit.vim",
 	"christoomey/vim-tmux-navigator",
-	"wincent/terminus",
 	"skywind3000/asyncrun.vim",
 	"tpope/vim-fugitive",
 	"tpope/vim-rhubarb",
 	{ "kyazdani42/nvim-tree.lua", dependencies = { "kyazdani42/nvim-web-devicons" } },
 
-	-- === Completion ===--
-	"neovim/nvim-lspconfig",
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-nvim-lua",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-  "hrsh7th/cmp-nvim-lsp-signature-help",
-  "onsails/lspkind-nvim",
 	"andersevenrud/compe-tmux",
 	"saadparwaiz1/cmp_luasnip",
 	"L3MON4D3/LuaSnip",
-  "simrat39/rust-tools.nvim",
+  	"simrat39/rust-tools.nvim",
   "rafamadriz/friendly-snippets",
 	"jose-elias-alvarez/null-ls.nvim",
 	"folke/trouble.nvim",
@@ -72,7 +264,7 @@ require("lazy").setup({
 	"lervag/vimtex",
 	"elmcast/elm-vim",
 	"pangloss/vim-javascript",
-  "mustache/vim-mustache-handlebars",
+  	"mustache/vim-mustache-handlebars",
 
 	-- === Visual ===--,
 	"luochen1990/rainbow",
@@ -80,9 +272,58 @@ require("lazy").setup({
 	"gruvbox-community/gruvbox",
 	"chriskempson/base16-vim",
 	"junegunn/goyo.vim",
-	{ "hoob3rt/lualine.nvim", dependencies = { { "kyazdani42/nvim-web-devicons", lazy = true } } },
-	"nvim-lua/lsp-status.nvim",
+	{
+	  "nvim-lualine/lualine.nvim",
+	  dependencies = { "nvim-tree/nvim-web-devicons" },
+	  event = "VeryLazy",
+	  config = function()
+	    -- === Colors / UI ===
+	    vim.g.base16colorspace = 256
+	    vim.o.termguicolors = true
+	    vim.cmd([[ highlight Comment cterm=italic ]])
+
+	    local lualine_theme
+	    if vim.g.color_is_light then
+	      lualine_theme = "onelight"
+	      vim.cmd("colorscheme base16-one-light")
+	    else
+	      lualine_theme = "gruvbox"
+	      vim.cmd("colorscheme gruvbox")
+	    end
+
+	    vim.cmd([[hi LineNr guibg=NONE]])
+	    vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+
+	    -- === Lualine ===
+	    require("lualine").setup({
+	      options = {
+	        theme = lualine_theme,
+	        globalstatus = true,
+	      },
+	      sections = {
+	        lualine_c = {
+	          "filename",
+	          {
+	            "lsp_progress", -- built-in, no deprecations
+	          },
+	        },
+	        lualine_x = {
+	          {
+	            "lsp_clients",
+	            icon = "",
+	          },
+	          "encoding",
+	          "filetype",
+	        },
+	      },
+	    })
+	  end,
+	},
+	{
+  		"j-hui/fidget.nvim",
+  		opts = {},
+	},
 	"folke/lsp-colors.nvim",
-  "RRethy/vim-illuminate"
+  	"RRethy/vim-illuminate"
 }
 )
